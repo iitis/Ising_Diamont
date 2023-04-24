3964b99979550dc5f6a07786ef1aa79dfd468716
zakaria <zakaria.mzaouali@gmail.com>
2023-04-24 06:59:20 +0200

    finite size calculations

--- exact diag/doapplyham.jl
+++ exact diag/doapplyham.jl
@@ -6,23 +6,21 @@ function doApplyHam(psiIn,hloc,N,usePBC)
     for k = 1:N-1
       # apply local Hamiltonian terms to sites [k,k+1]
       cont_inds = [[2],[2]];
-      psi_temp = tensordot(reshape(hloc,d^2,d^2),
-        reshape(psiIn,d^(k-1), d^2, d^(N-1-k)),cont_inds);
+      psi_temp = tensordot(reshape(hloc,d^2,d^2),reshape(psiIn,d^(k-1), d^2, d^(N-1-k)),cont_inds);
       psiOut = psiOut + reshape(permutedims(psi_temp,[2,1,3]),d^N);
     end
   
     if usePBC
       # apply periodic term
       cont_inds = [[3,4],[3,1]];
-      psi_temp = tensordot(reshape(hloc,d,d,d,d),
-        reshape(psiIn,d, d^(N-2), d),cont_inds);
+      psi_temp = tensordot(reshape(hloc,d,d,d,d), reshape(psiIn,d, d^(N-2), d), cont_inds)
       psiOut = psiOut + reshape(permutedims(psi_temp,[2,3,1]),d^N);
     end
   
     return psiOut
   end
 
-function tensordot(A, B, cont_inds)
+  function tensordot(A, B, cont_inds)
 
     A_free = deleteat!(collect(1:ndims(A)), sort(cont_inds[1]))
     B_free = deleteat!(collect(1:ndims(B)), sort(cont_inds[2]))
