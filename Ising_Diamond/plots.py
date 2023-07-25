f1b42b095582171a361b98ddeaad5a9be835f4ea
MoorishQubit <zakaria.mzaouali@gmail.com>
2023-07-25 15:24:13 +0200

    ising diamond

---
+++ Ising_Diamond/plots.py
@@ -0,0 +1,13 @@
+from Ising_Diamond.correlation_functions import *
+import matplotlib.pylab as plt
+
+plt.style.use('seaborn-v0_8-white')
+plt.rcParams.update({
+    "font.family": "serif",
+    "font.serif": ['Times New Roman'],
+})
+plt.rcParams['font.weight'] = 'normal'
+plt.rcParams['mathtext.fontset'] = 'stix'
+plt.tick_params(axis='both', which='major', labelsize=20)
+
+
