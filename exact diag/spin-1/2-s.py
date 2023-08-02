123f167347820dd40f9baab1dd3f15b4869b9e1b
zakaria <zakaria.mzaouali@gmail.com>
2023-08-02 10:09:26 +0200

    update

--- exact diag/spin-1/2-s.py
+++ exact diag/spin-1/2-s.py
@@ -15,25 +15,28 @@ plt.rcParams.update({
 })
 plt.rcParams['font.weight'] = 'normal'
 plt.rcParams['mathtext.fontset'] = 'stix'
-plt.tick_params(axis='both', which='major', labelsize=15)
+plt.tick_params(axis='both', which='major', labelsize=20)
 
 usePBC = True
 s=1
 d0 = 2 
 d1=int(2*s+1)
-d=d0*d1
-N=5
+d=d0*d1*d0
+N=6
 
 def Hamiltonian(h,s,N):
 
-    hloc = np.real(np.kron(np.array(sigmax()), np.array(jmat(s,'x')))+np.kron(np.array(sigmay()), np.array(jmat(s,'y')))+
-            np.kron(np.array(sigmaz()), np.array(jmat(s,'z')))- h * (np.kron(np.array(sigmaz()), np.array(qeye(d1)))+
-            np.kron(np.array(qeye(2)), np.array(jmat(s,'z'))))).reshape(2, d1, 2, d1)
-
+    # hloc = np.real(np.kron(np.array(sigmax()), np.array(jmat(s,'x')))+np.kron(np.array(sigmay()), np.array(jmat(s,'y')))+
+    #                np.kron(np.array(sigmaz()), np.array(jmat(s,'z')))- h * (np.kron(np.array(sigmaz()), np.array(qeye(d1)))+
+    #                                                                         np.kron(np.array(qeye(3)), np.array(jmat(s,'z'))))).reshape(2, d1, 2, d1)
+    hloc = np.real(np.array(tensor(sigmax(),jmat(s,'x'),qeye(2))+tensor(sigmay(),jmat(s,'y'),qeye(2))+tensor(sigmaz(),jmat(s,'z'),qeye(2))+
+            tensor(qeye(2),jmat(s,'x'),sigmax())+tensor(qeye(2),jmat(s,'y'),sigmay())+tensor(qeye(2),jmat(s,'z'),sigmaz())-
+            h * (tensor(sigmaz(),qeye(d1),qeye(2))+tensor(qeye(2),jmat(s,'z'),qeye(2)))).reshape(2, d1, 2, 2, d1, 2))
+    
     def doApplyHam(psiIn: np.ndarray, hloc: np.ndarray, N: int, usePBC: bool):
         d0 = hloc.shape[0]
         d1 = hloc.shape[1]
-        d = d0*d1
+        d = d0*d1*d0
         psiOut = np.zeros(psiIn.size)
         for k in range(N-1):
             psiOut += np.tensordot(hloc.reshape(d, d),
@@ -47,23 +50,23 @@ def Hamiltonian(h,s,N):
     def doApplyHamClosed(psiIn):
         return doApplyHam(psiIn, hloc, N, usePBC)
 
-    H = LinearOperator(((d0*d1)**N, (d0*d1)**N), matvec=doApplyHamClosed)
+    H = LinearOperator(((d0*d1*d0)**N, (d0*d1*d0)**N), matvec=doApplyHamClosed)
     Energy, psi = eigsh(H, k=1, which='SA')
     return psi
 
 def QI(h,s,N):
-    psi_r=Hamiltonian(h,s,N).reshape(2*d1,d**(N-1))
+    psi_r=Hamiltonian(h,s,N).reshape(d0*d1,d0*d**(N-1))
     psi_c=np.conj(psi_r)
     #rdm=Qobj(ncon([psi_r,psi_c],((-1,1),(-2,1))))
-    rdm=ncon([psi_r,psi_c],((-1,1),(-2,1)))
+    rdm=Qobj(ncon([psi_r,psi_c],((-1,1),(-2,1))))
     #rdm.dims=[[2,d1],[2,d1]]
-    return log_negativity(rdm,[2,d1])
+    return log_negativity(rdm,dim=[2,d1])
 
-h=np.linspace(0.1,5,100)
-
+h=np.linspace(0,4,100)
+#v=list(map(lambda h:Hamiltonian(h,s,N),h))
 v=list(map(lambda h:QI(h,s,N),h))
 
 plt.plot(h,v)
 plt.show()
 
-#print(Hamiltonian(1,1,5))
+#print(Hamiltonian(1,s,N))
