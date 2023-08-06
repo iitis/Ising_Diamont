4efacdb76dc3d8bc59413a46aefa728f2d65176a
zakaria <zakaria.mzaouali@gmail.com>
2023-08-07 00:30:34 +0200

    update

--- Ising_Diamond/correlation_functions.py
+++ Ising_Diamond/correlation_functions.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy import *
 import matplotlib.pylab as plt
+import numdifftools as nd
 
 ## Boltzmann weights according to spin-s (Eq. 51 - Valverde et al 2008)
 def w(s:str, mu:int, beta:float, J:float, h0:float, Jz:float, h:float, Jm:float, Jp:float):
@@ -19,10 +20,14 @@ def w(s:str, mu:int, beta:float, J:float, h0:float, Jz:float, h:float, Jm:float,
         x=np.exp(-beta*g)*(np.exp(beta*Jz)+2*np.exp(beta*Jz/2)*np.cosh(beta*np.sqrt(Jz**2 + 2*Jp**2)/2)+4*np.cosh(beta*Jp/2)*np.cosh(beta*l)+
                                 2*np.exp(-beta*Jz)*np.cosh(beta*2*l))
     return x
+
+## Free energy: log of the largest eigenvalue of the transfert matrix (thermodynamic limit) 
+def F(s:str, beta:float, J:float, h0:float, Jz:float, h:float, Jm:float, Jp:float):
+    B=np.sqrt((w(s,1,beta,J,h0,Jz,h,Jm,Jp)-w(s,-1,beta,J,h0,Jz,h,Jm,Jp))**2 + 4*w(s,0,beta,J,h0,Jz,h,Jm,Jp)**2)
+    lp=(w(s,1,beta,J,h0,Jz,h,Jm,Jp)+w(s,-1,beta,J,h0,Jz,h,Jm,Jp)+B)/2
+    free_energy=(-1/beta) * np.log(lp)
+    return free_energy
     
-
-s='1'; mu=1; beta=1; J=1.0; h0=0.5; Jz=1.0; h=0.3; Jm=0.6; Jp=5.0
-
 ## magnetization <spin-s> (Eq. 30 - Carvalho et al 2019)
 def mag(beta:float, J:float, h0:float, Jz:float, h:float, Jm:float, Jp:float):
     s='1/2'
@@ -37,17 +42,44 @@ def sigma_i_sigma_j(beta:float,r:int, J:float, h0:float, Jz:float, h:float, Jm:f
     sisj=mag(beta,J,h0,Jz,h,Jm,Jp)**2 + (w(s,0,beta,J,h0,Jz,h,Jm,Jp)/B)**2 * ((w(s,1,beta,J,h0,Jz,h,Jm,Jp)+w(s,-1,beta,J,h0,Jz,h,Jm,Jp)-B)/(w(s,1,beta,J,h0,Jz,h,Jm,Jp)+w(s,-1,beta,J,h0,Jz,h,Jm,Jp)+B))**r
     return sisj
 
-## Heisenberg spin-spin correlation function in the z direction (Eq. 34 - Carvalho et al 2019)
-def Sz(beta:float, J:float, h0:float, Jz:float, h:float, Jm:float, Jp:float):
-    s='1'
-    B=np.sqrt((w(s,1,beta,J,h0,Jz,h,Jm,Jp)-w(s,-1,beta,J,h0,Jz,h,Jm,Jp))**2 + 4*w(s,0,beta,J,h0,Jz,h,Jm,Jp)**2)
-    lp=(w(s,1,beta,J,h0,Jz,h,Jm,Jp)+w(s,-1,beta,J,h0,Jz,h,Jm,Jp)+B)/2
-    wz_p=wz_mu(s,1,beta,J,h0,Jz,h,Jm,Jp)
-    wz_m=wz_mu(s,-1,beta,J,h0,Jz,h,Jm,Jp)
-    wz_0=wz_mu(s,0,beta,J,h0,Jz,h,Jm,Jp)
-    sz=(wz_p + wz_m)/(2*lp) + ((wz_p - wz_m)*(w(s,1,beta,J,h0,Jz,h,Jm,Jp)-w(s,-1,beta,J,h0,Jz,h,Jm,Jp)))/(2*B*lp) + (2*w(s,0,beta,J,h0,Jz,h,Jm,Jp)*wz_0)/(B*lp)
+
+def Sz(s:str,beta:float, J:float, h0:float, Jz:float, h:float, Jm:float, Jp:float):
+    a=lambda h:F(s,beta,J,h0,Jz,h,Jm,Jp)
+    da=nd.Derivative(a,n=1)
+    df=da(h)
+    sz=df/abs(df).max()
     return sz
 
+def SzSz(beta:float, J:float, h0:float, Jz:float, h:float, Jm:float, Jp:float):
+    a=lambda Jz : F('1',beta,J,h0,Jz,h,Jm,Jp)
+    da=nd.Derivative(a,n=1)
+    df=da(Jz)
+    szsz=df/abs(df).max()
+    return szsz
 
+def SxSx(beta:float, J:float, h0:float, Jz:float, h:float, Jp:float):
+    a=lambda Jp : F('1',beta,J,h0,Jz,h,0,Jp)
+    da=nd.Derivative(a,n=1)
+    df=da(Jp)
+    sxsx=2 * df/abs(df).max()
+    return sxsx
 
-#print(sigma_i_sigma_j(s,1,beta,J,h0,Jz,h,Jm,Jp))
+def Shsi(s,beta,J,h0,Jz,h,Jm,Jp):
+    df= nd.Derivative(lambda h : np.log(w(s,0,beta,J,h0,Jz,h,Jm,Jp) * np.sqrt(w(s,1,beta,J,h0,Jz,h,Jm,Jp) * w(s,-1,beta,J,h0,Jz,h,Jm,Jp))),n=1)
+    q0=-(1/(2*beta)) * df(h)#/abs(df(h)).max()
+    dB=nd.Derivative( lambda h: -(1/(2*beta)) * np.log(w(s,1,beta,J,h0,Jz,h,Jm,Jp)/w(s,-1,beta,J,h0,Jz,h,Jm,Jp)) ,n=1)
+    q10=(1/2) * dB(h)#/abs(dB(h)).max()
+    dK=nd.Derivative( lambda h: -(4/beta) * np.log(w(s,1,beta,J,h0,Jz,h,Jm,Jp)*w(s,-1,beta,J,h0,Jz,h,Jm,Jp)/(w(s,0,beta,J,h0,Jz,h,Jm,Jp)**2)) ,n=1)
+    q11=(1/8) * dK(h)#/abs(dK(h)).max()
+
+    B=np.sqrt((w(s,1,beta,J,h0,Jz,h,Jm,Jp)-w(s,-1,beta,J,h0,Jz,h,Jm,Jp))**2 + 4*w(s,0,beta,J,h0,Jz,h,Jm,Jp)**2)
+    l=(w(s,1,beta,J,h0,Jz,h,Jm,Jp)+w(s,-1,beta,J,h0,Jz,h,Jm,Jp)+B)/2
+    corr=np.log(l) ## this is 1/correlation length
+    mixed_spin=(q0*mag(beta,J,h0,Jz,h,Jm,Jp) + 2 * q10 * (mag(beta,J,h0,Jz,h,Jm,Jp)**2 + (1 - mag(beta,J,h0,Jz,h,Jm,Jp)**2) * np.exp(-corr) ) +
+                q11 *  ( mag(beta,J,h0,Jz,h,Jm,Jp)**3 + mag(beta,J,h0,Jz,h,Jm,Jp) * (1 - mag(beta,J,h0,Jz,h,Jm,Jp)**2) * (1+2*np.exp(-corr)) ) )
+    return mixed_spin
+  
+
+# s='1'; beta=100; J=1.0; h0=0.0; Jz=0.5; h=0; Jm=1; Jp=np.linspace(0,1,10)
+
+# print(Sz(beta,J,h0,Jz,h,Jm,Jp))
--- Ising_Diamond/wigner.py
+++ Ising_Diamond/wigner.py
@@ -1,10 +1,26 @@
 import numpy as np
+from correlation_functions import *
 from qutip import *
-
-def rho():
-    return state
+from toqito.state_props import l1_norm_coherence, negativity, log_negativity
 
 
-def wigner():
+def rho(s,beta,J,h0,Jz,h,Jm,Jp):
+    d=2
+    id=tensor(qeye(2),qeye(d),qeye(d),qeye(2))
+    single_site= mag(beta,J,h0,Jz,h,Jm,Jp)*(tensor(sigmaz(),qeye(d),qeye(d),qeye(2))+tensor(qeye(2),qeye(d),qeye(d),sigmaz())) + Sz(s,beta,J,h0,Jz,h,Jm,Jp) * (tensor(qeye(2),jmat(1/2,'z'),qeye(d),qeye(2))+tensor(qeye(2),qeye(d),jmat(1/2,'z'),qeye(2)))
     
-    return w
+    two_site = (sigma_i_sigma_j(beta,1,J,h0,Jz,h,Jm,Jp) * tensor(sigmaz(),qeye(d),qeye(d),sigmaz()) + SzSz(beta,J,h0,Jz,h,Jm,Jp) * tensor(qeye(2),jmat(1/2,'z'),jmat(1/2,'z'),qeye(2)) + 
+                2 * SxSx(beta,J,h0,Jz,h,Jp) * tensor(qeye(2),jmat(1/2,'x'),jmat(1/2,'x'),qeye(2)))
+    
+    three_site = mag(beta,J,h0,Jz,h,Jm,Jp)*Shsi(s,beta,J,h0,Jz,h,Jm,Jp)*(tensor(sigmaz(),jmat(1/2,'z'),qeye(d),sigmaz()) + 
+                                                                         tensor(sigmaz(),qeye(d),jmat(1/2,'z'),sigmaz())) 
+    
+    four_site = sigma_i_sigma_j(beta,1,J,h0,Jz,h,Jm,Jp) * SzSz(beta,J,h0,Jz,h,Jm,Jp) * tensor(sigmaz(),jmat(1/2,'z'),jmat(1/2,'z'),sigmaz())
+
+    state=  (1/(2*d*2*d)) * (id + single_site + two_site + three_site + four_site)
+    return log_negativity(np.array(state),[2,2,2,2])
+
+s='1/2'; beta=1; h0=h=0
+#print(rho(s,beta,1,h0,1,h,2,1))
+
+
