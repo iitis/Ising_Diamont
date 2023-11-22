7b1f89d023ad71cbbb8b26ee568e488a03b41275
MoorishQubit <zakaria.mzaouali@gmail.com>
2023-11-22 11:45:19 +0100

    added lbc for 4 and 3 qubit in mixed spin chain

--- Ising_Diamond/funcs.py
+++ Ising_Diamond/funcs.py
@@ -4,9 +4,8 @@ from qutip import *
 from toqito.state_props import l1_norm_coherence, negativity, log_negativity
 from toqito.state_props import concurrence as cnc
 import statistics
-from scipy.ndimage import gaussian_filter
+from scipy.ndimage import gaussian_filter1d
 from scipy.stats import entropy
-from scipy.stats import ortho_group
 
 def rho(s,beta,J,h0,Jz,h,Jp):
     #s,beta,J,h0,Jz,h,Jp = args
@@ -65,10 +64,11 @@ def neg(args):
 def wigner_entropy(args):
     s, beta, J, h0, Jz, h, Jp = args
     t=np.linspace(0,np.pi/2,50); f=np.linspace(0,2*np.pi,50)
-    data=list(map(lambda t,f: wigner(t,f,s, beta, J, h0, Jz, h, Jp),t,f))
-    sigma=15
-    wigner_smoothed = gaussian_filter(data, sigma)
-    return entropy(wigner_smoothed,base=2)
+    data=list(wigner(ti, fi, s, beta, J, h0, Jz, h, Jp) for ti in t for fi in f)
+    sigma=100
+    wigner_smoothed = gaussian_filter1d(data, sigma, order=0, mode="mirror")
+    #wigner_smoothed_normalized = wigner_smoothed / sum(wigner_smoothed)
+    return entropy(wigner_smoothed,base=2)*(np.pi**2)/100
 
 def so(n):
     """
@@ -90,7 +90,7 @@ def so(n):
             generators.append(Qobj(generator))
     return generators
 
-def lbc_3_sites(args):
+def lbc_3_qubits(args):
     """
     Compute the lower bound concurrence for a system of three qubits as described by Ming Li et al 2009 J. Phys. A: Math. Theor. 42 145303
     
@@ -120,7 +120,7 @@ def lbc_3_sites(args):
     lbc = (sum(c123) + sum(c132)+ sum(c231))/3
     return np.sqrt(lbc)
 
-def lbc_4_sites(args):
+def lbc_4_qubits(args):
     """
     Compute the lower bound concurrence for a system of four qubits as described by Ming Li et al 2009 J. Phys. A: Math. Theor. 42 145303
     
@@ -162,3 +162,91 @@ def lbc_4_sites(args):
             c14_23.append(max(0,np.sqrt(eigen14_23[0])-np.sqrt(eigen14_23[1])-np.sqrt(eigen14_23[2])-np.sqrt(eigen14_23[3]))**2)
     lbc = (sum(c1_234) + sum(c2_134) + sum(c3_124) + sum(c4_123) + sum(c12_34) + sum(c13_24) + sum(c14_23) + sum(c12_34) + sum(c13_24) + sum(c14_23))/7
     return np.sqrt(lbc)
+
+def lbc_3_mixed(args):
+    """
+    Compute the lower bound concurrence for a system of three qubits as described by Ming Li et al 2009 J. Phys. A: Math. Theor. 42 145303
+    
+    Parameters:
+    s (string): spin half or one
+    beta (float): inverse temperature
+    J (float): coupling between Ising nodes
+    h0, h (float): magnetic field
+    Jz (float): coupling between Heisenberg nodes in the z-direction
+    Jp (float): coupling between Heisenberg nodes in the x- and y-direction; Jp=Jx+Jy
+
+    Returns:
+    float: value of the lower bound concurrence
+    """
+    s,beta,J,h0,Jz,h,Jp=args
+    c123=[]; c132=[]; c231=[]
+    state=rho(s,beta,J,h0,Jz,h,Jp).ptrace([0,1,2])
+    for gen1 in so(6):
+        for gen2 in so(3):
+            s123=tensor(gen1,gen2)
+            s123.dims=[[2,3,3],[2,3,3]]
+            eigen123=(state*(s123*state.conj()*s123)).eigenenergies(sort='high')
+            eigen132=((state.permute([0,2,1]))*(s123*(state.permute([0,2,1])).conj()*s123)).eigenenergies(sort='high')
+            c123.append(max(0,np.sqrt(eigen123[0])-np.sqrt(eigen123[1])-np.sqrt(eigen123[2])-np.sqrt(eigen123[3]))**2)
+            c132.append(max(0,np.sqrt(eigen132[0])-np.sqrt(eigen132[1])-np.sqrt(eigen132[2])-np.sqrt(eigen132[3]))**2)
+    for gen3 in so(9):
+        s231=tensor(gen3,so(2)[0])
+        s231.dims=[[3,3,2],[3,3,2]]
+        eigen231=((state.permute([1,2,0]))*(s231*(state.permute([1,2,0])).conj()*s231)).eigenenergies(sort='high')
+        c231.append(max(0,np.sqrt(eigen231[0])-np.sqrt(eigen231[1])-np.sqrt(eigen231[2])-np.sqrt(eigen231[3]))**2)
+    lbc = (sum(c123) + sum(c132)+ sum(c231))/3
+    return np.sqrt(lbc)
+
+def lbc_4_mixed(args):
+    """
+    Compute the lower bound concurrence for a system of four qubits as described by Ming Li et al 2009 J. Phys. A: Math. Theor. 42 145303
+    
+    Parameters:
+    s (string): spin half or one
+    beta (float): inverse temperature
+    J (float): coupling between Ising nodes
+    h0, h (float): magnetic field
+    Jz (float): coupling between Heisenberg nodes in the z-direction
+    Jp (float): coupling between Heisenberg nodes in the x- and y-direction; Jp=Jx+Jy
+
+    Returns:
+    float: value of the lower bound concurrence
+    """
+    s,beta,J,h0,Jz,h,Jp=args
+    c1_234=[]; c2_134=[]; c3_124=[]; c4_123=[]
+    c12_34=[]; c13_24=[]; c14_23=[]
+    state=rho(s,beta,J,h0,Jz,h,Jp)
+    for gen1 in so(18):
+        s1_p=tensor(so(2)[0],gen1)
+        s1_p.dims=[[2,3,2,3],[2,3,2,3]]
+        eigen1_234=(state*(s1_p*state.conj()*s1_p)).eigenenergies(sort='high')
+        eigen4_123=((state.permute([3,0,1,2]))*(s1_p*(state.permute([3,0,1,2])).conj()*s1_p)).eigenenergies(sort='high')
+        c1_234.append(max(0,np.sqrt(eigen1_234[0])-np.sqrt(eigen1_234[1])-np.sqrt(eigen1_234[2])-np.sqrt(eigen1_234[3]))**2)
+        c4_123.append(max(0,np.sqrt(eigen4_123[0])-np.sqrt(eigen4_123[1])-np.sqrt(eigen4_123[2])-np.sqrt(eigen4_123[3]))**2)
+    for gen2 in so(3):
+        for gen3 in so(12):
+            s2_p=tensor(gen2,gen3)
+            s2_p.dims=[[2,3,2,3],[2,3,2,3]]
+            eigen2_134=((state.permute([1,0,2,3]))*(s2_p*(state.permute([1,0,2,3])).conj()*s2_p)).eigenenergies(sort='high')    
+            eigen3_124=((state.permute([2,0,1,3]))*(s2_p*(state.permute([2,0,1,3])).conj()*s2_p)).eigenenergies(sort='high')    
+            c2_134.append(max(0,np.sqrt(eigen2_134[0])-np.sqrt(eigen2_134[1])-np.sqrt(eigen2_134[2])-np.sqrt(eigen2_134[3]))**2)
+            c3_124.append(max(0,np.sqrt(eigen3_124[0])-np.sqrt(eigen3_124[1])-np.sqrt(eigen3_124[2])-np.sqrt(eigen3_124[3]))**2)
+    for gen4 in so(6):
+        for gen5 in so(6):
+            Sij_kl=tensor(gen4,gen5)
+            Sij_kl.dims=[[2,3,2,3],[2,3,2,3]]
+            eigen12_34=(state*(Sij_kl*state.conj()*Sij_kl)).eigenenergies(sort='high')
+            eigen13_24=((state.permute([0,2,1,3]))*(Sij_kl*(state.permute([0,2,1,3])).conj()*Sij_kl)).eigenenergies(sort='high')
+            c12_34.append(max(0,np.sqrt(eigen12_34[0])-np.sqrt(eigen12_34[1])-np.sqrt(eigen12_34[2])-np.sqrt(eigen12_34[3]))**2)
+            c13_24.append(max(0,np.sqrt(eigen13_24[0])-np.sqrt(eigen13_24[1])-np.sqrt(eigen13_24[2])-np.sqrt(eigen13_24[3]))**2)
+    for gen6 in so(4):
+        for gen7 in so(9):
+            s14_23=tensor(gen6,gen7)
+            s14_23.dims=[[2,3,2,3],[2,3,2,3]]
+            eigen14_23=((state.permute([0,3,1,2]))*(s14_23*(state.permute([0,3,1,2])).conj()*s14_23)).eigenenergies(sort='high')
+            c14_23.append(max(0,np.sqrt(eigen14_23[0])-np.sqrt(eigen14_23[1])-np.sqrt(eigen14_23[2])-np.sqrt(eigen14_23[3]))**2)
+    lbc = (sum(c1_234) + sum(c2_134) + sum(c3_124) + sum(c4_123) + sum(c12_34) + sum(c13_24) + sum(c14_23) + sum(c12_34) + sum(c13_24) + sum(c14_23))/7
+    return np.sqrt(lbc)
+
+# args=['1/2',300,1,1e-4,1,1e-4,1]
+# print(wigner_entropy(args))
